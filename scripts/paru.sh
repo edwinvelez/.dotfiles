b900ad5dddcabc898b92e9b014716dc30347adca
edwinvelez <2299753+edwinvelez@users.noreply.github.com>
2024-03-23 15:06:30 -0400

    first commit

---
+++ scripts/paru.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Installing paru"
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si --noconfirm --needed
+
+echo "Cleaning up paru build directory"
+cd ..
+rm -rf paru
+
+echo "Installing google-chrome"
+paru -S google-chrome --noconfirm --needed
+
+echo "Installing Dropbox"
+paru -S dropbox --noconfirm --needed
