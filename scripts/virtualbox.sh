87ef1d9eecab97b392e8015ebb83f134b65c03aa
edwinvelez <2299753+edwinvelez@users.noreply.github.com>
2024-03-23 14:42:47 -0400

    first commit

---
+++ scripts/virtualbox.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Installing VirtualBox"
+sudo pacman -S \
+virtualbox \
+virtualbox-host-modules-arch \
+virtualbox-guest-iso \
+--noconfirm --needed
+
+echo "Adding user to vboxusers group"
+sudo usermod -aG vboxusers $USER
+
+echo "Installing VirtualBox extensions"
+paru -S virtualbox-ext-oracle --noconfirm
