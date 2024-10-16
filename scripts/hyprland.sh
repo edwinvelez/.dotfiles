d718d335cb690fdce51af32ae70dafb709f36ae5
edwinvelez <2299753+edwinvelez@users.noreply.github.com>
2024-10-16 15:53:17 -0400

    first commit

---
+++ scripts/hyprland.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Installing Wayland
+pacman -S \
+brightnessctl \
+kitty \
+nvidia \
+nvidia-lts \
+nvidia-settings \
+nvidia-utils \
+nwq-look \
+pipewire \
+qt5ct \
+qt6ct \
+ranger \
+rofi-wayland \
+waybar \
+wireplumber \
+xdg-desktop-portal-hyprland \
+xdg-desktop-portal-gtk \
